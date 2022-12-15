0aac4a38f0bebc84e8c1da4412ddc515ce348f4f
rutaji <ruta.jirka@centrum.cz>
2022-12-15 17:48:04 +0100

    done

---
+++ AoC2022/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class MainClass 
+{
+    static int[] Max = new int[3];
+    static void Main() 
+    {
+        
+        int CurentElf = 0;
+        foreach(string Line in File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Input.txt"))) 
+        {
+            if (Line == "") { if (CurentElf > Max[0]) { Change(CurentElf); } CurentElf = 0; }
+            else
+            {
+                CurentElf += int.Parse(Line);
+            }
+        }
+        if (CurentElf > Max[0]) { Change(CurentElf); }
+        int sum = 0;
+        foreach(int s in Max) { sum += s; }
+        Console.WriteLine(sum);
+    }
+    static void Change(int i) 
+    {
+        Max[0] = i;
+        Array.Sort(Max);
+    }
+
+}

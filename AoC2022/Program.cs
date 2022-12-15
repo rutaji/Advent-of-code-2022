137a7a1a7ee1969cbbfc2ba687cf2099af2b1f2d
rutaji <ruta.jirka@centrum.cz>
2022-12-15 17:37:50 +0100

    done

---
+++ AoC2022/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class MainClass 
+{
+    static void Main() 
+    {
+        int Max = 0;
+        int CurentElf = 0;
+        foreach(string Line in File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Input.txt"))) 
+        {
+            if (Line == "") { if (CurentElf > Max) { Max = CurentElf; } CurentElf = 0; }
+            else
+            {
+                CurentElf += int.Parse(Line);
+            }
+        }
+        if (CurentElf > Max) { Max = CurentElf; }
+        Console.WriteLine(Max);
+    }
+}

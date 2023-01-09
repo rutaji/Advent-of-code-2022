04807a7e72d0bc3630db580cf78b898d2d210dd1
rutaji <jiri.ruta@tul.cz>
2023-01-09 17:07:00 +0100

    done

---
+++ 2022AOC06/Program.cs
@@ -0,0 +1,13 @@
+﻿class main
+{
+    static void Main()
+    {
+        char[] c = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Input.txt")).ToCharArray();
+        for(int i = 3;i < c.Length;i++){
+            if (c[i] != c[i - 1] && c[i] != c[i-2] && c[i] != c[i-3] &&
+                c[i - 1] != c[i - 2] && c[i-1] != c[i-3] &&
+                c[i - 2] != c[i - 3]) 
+            { Console.WriteLine(i+1);return; }
+        }
+    }
+}

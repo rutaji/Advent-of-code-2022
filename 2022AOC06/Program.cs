f4ed4aa87e294847c1d68a51b36f977268078e77
rutaji <jiri.ruta@tul.cz>
2023-01-17 22:25:19 +0100

    done with buffer

--- 2022AOC06/Program.cs
+++ 2022AOC06/Program.cs
@@ -1,13 +1,65 @@
-﻿class main
+﻿using System.Globalization;
+
+class CircularBuffer
+{
+    int[] Values;
+    int read = 0;
+    int write;
+    int max;
+    public CircularBuffer(int length) { this.max = length - 1; Values = new int[length]; write = length - 1; }
+
+
+    public int Read()
+    {
+        int value = Values[read++];
+        if (read > max) { read = 0; }
+        return value;
+    }
+    public void Write(int value)
+    {
+        Values[write++] = value;
+        if (write > max) { write = 0; }
+    }
+    public int[] GetValues()
+    {
+        return Values;
+    }
+}
+
+class main
 {
     static void Main()
     {
-        char[] c = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Input.txt")).ToCharArray();
-        for(int i = 3;i < c.Length;i++){
-            if (c[i] != c[i - 1] && c[i] != c[i-2] && c[i] != c[i-3] &&
-                c[i - 1] != c[i - 2] && c[i-1] != c[i-3] &&
-                c[i - 2] != c[i - 3]) 
-            { Console.WriteLine(i+1);return; }
+        int lenght = 4;
+        CircularBuffer Buffer = new(lenght);
+        StreamReader Reader = new(Path.Combine(Environment.CurrentDirectory, "Input.txt"));
+        for(int i = 0; i < lenght-1; i++) 
+        {
+            Buffer.Write(Reader.Read());
         }
+        int index = lenght;
+        do
+        {
+            
+            Buffer.Write(Reader.Read());
+            if (AllDiferent(Buffer.GetValues())) { break; }
+            index++;
+        }while(!Reader.EndOfStream);
+        Console.WriteLine(index);
+           
+        
+    }
+    static bool AllDiferent(int[] input) 
+    {
+        int first;
+        for(int i = 0; i < input.Length ; i++)
+        {
+            first = input[i];
+            for(int j = i+1; j < input.Length ; j++) 
+            {
+                if (input[j] == first) { return false; }
+            }
+        }
+        return true;
     }
 }

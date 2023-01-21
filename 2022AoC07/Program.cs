f2eb5105b313764f06695c9d7056cfe9db8180f0
rutaji <jiri.ruta@tul.cz>
2023-01-21 17:59:04 +0100

    done

---
+++ 2022AoC07/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+class Folder
+{
+    public Dictionary<string,Folder> Folders= new();
+    public int sizeOfFiles = 0;
+    public Folder parent;
+    private int WholeSize = 0;
+    public Folder(Folder p)
+    {
+        parent = p;
+    }
+    public int GetwholeSize() 
+    {
+        if(WholeSize != 0) { return WholeSize; }
+        foreach(Folder p in Folders.Values) 
+        {
+           WholeSize += p.GetwholeSize();
+        }
+        WholeSize += sizeOfFiles;
+        return WholeSize;
+    }
+    
+}
+class main 
+{
+
+
+    static Folder root = new Folder(null);
+    static Folder curent = root;
+
+   static int state = 1;
+    static public void cd(string name) 
+    {
+        if(name == "..") { curent = curent.parent; return; }
+        if(name == "/") { curent = root;return; }
+        if (!curent.Folders.ContainsKey(name)) { curent.Folders.Add(name, new Folder(curent)); }
+        curent = curent.Folders[name];
+    }
+    static public void ls() 
+    {
+        curent.sizeOfFiles= 0;
+        state = 0;
+    }
+    static public void Main() 
+    {
+       
+        foreach(string input in File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Input.txt"))) 
+        {
+           string[] Inputs = input.Split(' ');
+            
+            
+            if (state == 0)
+            {
+                if (Inputs[0] == "$") { state = 1; }
+                else if(Inputs[0] != "dir"){ curent.sizeOfFiles += int.Parse(Inputs[0]); }
+                else if (!curent.Folders.ContainsKey(Inputs[1])){ curent.Folders.Add(Inputs[1], new Folder(curent)); }
+            }
+
+            if(state == 1)
+            { 
+                 if (Inputs[1] == "cd") { cd(Inputs[2]); }
+                 else if (Inputs[1] == "ls") { ls(); }
+
+            }
+        }
+        root.GetwholeSize();
+        List<Folder> cool = GetAll(root, x => x.GetwholeSize() <= 100000);
+        int suma = 0;
+        foreach(Folder folder in cool) {suma += folder.GetwholeSize(); }
+        Console.WriteLine(suma);
+
+    }
+    
+    static List<Folder> GetAll(Folder start,Func<Folder,bool> filtr) 
+    {
+        List<Folder> list = new List<Folder>();
+        if (filtr(start)) { list.Add(start); }
+        foreach(Folder f in start.Folders.Values) 
+        {
+            foreach(Folder done in GetAll(f, filtr)) 
+            {
+                list.Add(done);
+            } 
+        }
+        return list;
+    }
+
+}
